465553fdc014cfc5de7e4fae4dba2dd2a8df6ba5
filvet80 <70665320+filvet80@users.noreply.github.com>
2020-10-19 22:10:24 +0300

    1

---
+++ Переменные.py
@@ -0,0 +1,17 @@
+Car_model = "bmv"
+Car_color = "black"
+Year_of_produce = 2018
+Count_of_doors = 5
+print(Car_model)
+print(Car_color)
+print(Year_of_produce)
+print(Count_of_doors)
+
+person_1_age=53
+person_2_age=78
+person_3_age=23
+person_4_age=42
+person_5_age=81
+count_of_person=5
+averege_age=(person_1_age+person_2_age+person_3_age+person_4_age+person_5_age)/count_of_person
+print(averege_age)
